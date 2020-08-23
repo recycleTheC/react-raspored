0a61b1d5cf236a5c9b8d7327b126772f3a26e8ec
recycleTheC <mario.kopjar1@gmail.com>
2020-08-23 22:01:17 +0200

    Update DailySchedule.jsx

--- src/components/DailySchedule.jsx
+++ src/components/DailySchedule.jsx
@@ -6,11 +6,6 @@ function DailySchedule() {
   const context = useContext(ScheduleContext);
   const { schedule, notes } = context;
 
-  /**
-   * @todo Refactoring 'DailySchedule' component
-   * @body Component should be constructed from few smaller components (`ScheduleItem`, etc.)
-   */
-
   return (
     <ListGroup variant="flush">
       {schedule.map((x) => {
