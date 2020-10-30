852f402bc9e8aab423800a73590e9b3098dc3cc4
recycleTheC <mario.kopjar1@gmail.com>
2020-10-30 09:43:45 +0100

    Fix private route

--- src/components/routing/PrivateRoute.jsx
+++ src/components/routing/PrivateRoute.jsx
@@ -1,24 +1,25 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import AuthContext from '../../context/auth/authContext';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 	const authContext = useContext(AuthContext);
-	const { loading, isAuthenticated } = authContext;
+	const { isAuthenticated } = authContext;
 
 	return (
 		<Route
 			{...rest}
 			render={(props) =>
-				!isAuthenticated && !loading ? (
-					<Redirect to='/login' />
-				) : (
-					<Component {...props} />
-				)
+				!isAuthenticated ? <Redirect to='/login' /> : <Component {...props} />
 			}
 		/>
 	);
+};
+
+PrivateRoute.propTypes = {
+	component: PropTypes.node,
 };
 
 export default PrivateRoute;
