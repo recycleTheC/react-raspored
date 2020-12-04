2f8ce3724f4252b2c9ab9a16a5f420af261da8c8
recycleTheC <mario.kopjar1@gmail.com>
2020-12-04 14:26:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,17 @@ import AddClass from './components/classes/AddClass';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
 import Timeline from './components/timeline/Timeline';
+import NotFound from './pages/NotFound';
+import Notifications from './pages/notifications/Notifications';
+import Notification from './pages/notifications/Notification';
+import EditNotification from './pages/notifications/EditNotification';
 
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token);
 }
+
 function App() {
 	return (
 		<BrowserRouter>
@@ -37,6 +42,19 @@ function App() {
 								<PrivateRoute exact path='/class/add' component={AddClass} />
 								<Route exact path='/login' component={Login} />
 								<Route exact path='/notes' component={Timeline} />
+								<Route exact path='/notifications' component={Notifications} />
+								<Route
+									exact
+									path='/notifications/:id'
+									component={Notification}
+								/>
+								<PrivateRoute
+									exact
+									path='/notifications/edit/:id'
+									component={EditNotification}
+								/>
+								<Route component={NotFound} />{' '}
+								{/* MUST be at the END of the switch*/}
 							</Switch>
 						</Layout>
 					</AlertState>
