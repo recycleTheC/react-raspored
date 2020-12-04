686278887171acd0ab6878029331d2dabe05c63b
recycleTheC <mario.kopjar1@gmail.com>
2020-12-04 14:26:48 +0100

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotFound = () => {
+	return (
+		<div>
+			<p>
+				<strong>Ups! Ne mogu pronaći stranicu koju ste tražili :(</strong>
+			</p>
+		</div>
+	);
+};
+
+export default NotFound;
